7e4314e4715a3d06060622952535787fb921fc56
ARJUNGENIUS <akhilarjunganesh@gmail.com>
2023-10-27 16:45:54 -0400

    Updates repositories for OpenCV, FTCLib, and RoadRunner.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SayWattTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SayWattTeleOp.java
@@ -5,16 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="Say Watt TeleOp")
+@TeleOp (name = "TeleOp")
 public class SayWattTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-
+      
         DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
         DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
         DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRight = hardwareMap.dcMotor.get("backRight");
-
 
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -25,10 +24,12 @@ public class SayWattTeleOp extends LinearOpMode {
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
